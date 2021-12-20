effc5cc12758e2383c2f44ed643c923970715728
streetmask <863623116@qq.com>
2021-12-20 16:06:25 +0800

    暂时的初始页面

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,14 @@
   windows_subsystem = "windows"
 )]
 
+#[tauri::command]
+fn my_custom_command() -> String {
+  "Hello from Rust!".into()
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![my_custom_command])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
