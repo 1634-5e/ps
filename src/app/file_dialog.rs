16ad3471ed8f05b1ca78dd988e6f703fb67767bd
streetmask <863623116@qq.com>
2022-03-27 17:11:36 +0800

    添加了保存时默认文件名为当前时间

--- src/app/file_dialog.rs
+++ src/app/file_dialog.rs
@@ -1,3 +1,4 @@
+use chrono::{Datelike, Local, Timelike};
 pub use native_dialog::FileDialog;
 pub use std::path::PathBuf;
 
@@ -21,8 +22,25 @@ pub fn pick(dialog_type: DialogType) -> Option<PathBuf> {
 }
 
 pub fn save() -> Option<PathBuf> {
+    let now = Local::now();
+
+    let (is_pm, hour) = now.hour12();
+    let (_, year) = now.year_ce();
+    let filename = format!(
+        "{}-{:02}-{:02} {:?} {:02}-{:02}-{:02} {}",
+        year,
+        now.month(),
+        now.day(),
+        now.weekday(),
+        hour,
+        now.minute(),
+        now.second(),
+        if is_pm { "PM" } else { "AM" }
+    );
+
     FileDialog::new()
         .set_location("D://Desktop")
+        .set_filename(filename.as_str())
         .add_filter("SVG Image", &["svg"])
         .show_save_single_file()
         .unwrap()
